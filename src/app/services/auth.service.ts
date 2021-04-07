5b05a977ce8fb2930755fee1d78a9a86ac4b9949
Sabdiel <72673576+Sabdiel@users.noreply.github.com>
2021-04-07 12:30:46 -0500

    actualizacion

--- src/app/services/auth.service.ts
+++ src/app/services/auth.service.ts
@@ -6,7 +6,7 @@ import { HttpClient } from '@angular/common/http'
 })
 export class AuthService {
 
-  private URL = "https://api-pagos-utxj.herokuapp.com/api"
+  private URL = "https://api-pagos-utxj.herokuapp.com/api "
 
   constructor(private http: HttpClient) { }
 
